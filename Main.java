50fb6e0db942aa34740c7008db745e7cec39380b
fehnicchio <63413139+fehnicchio@users.noreply.github.com>
2021-11-24 16:57:44 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Por favor digite o cnpj: ");
+        System.out.println("Por favor, digite o cnpj: ");
         Scanner cnpj = new Scanner( System.in ); //recebe a entrada do teclado
         CNPJ.validarCnpj(cnpj.nextLine()); //chama o metodo para a validaçao do CNPJ
     }
